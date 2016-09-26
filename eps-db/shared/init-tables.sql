effdb2ba047034f7d06daa4c89eb8e2b07fb7f01
Zoltan Ferenczik <zoltan.ferenczik@icellmobilsoft.hu>
2016-09-26 14:23:06 +0200

    New init script (customer + vehicle tables)

--- eps-db/shared/init-tables.sql
+++ eps-db/shared/init-tables.sql
@@ -1,6 +1,6 @@
 -- MySQL dump 10.13  Distrib 5.7.15, for Linux (x86_64)
 --
--- Host: 172.18.0.2    Database: mysql
+-- Host: 172.18.0.3    Database: mysql
 -- ------------------------------------------------------
 -- Server version	5.7.15
 
@@ -15,29 +15,60 @@
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
+--
+-- Using database `mysql`
+--
 USE `mysql`;
+
 --
 -- Table structure for table `customer`
 --
-
 DROP TABLE IF EXISTS `customer`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `customer` (
-  `CUST_ID` int(10) unsigned NOT NULL AUTO_INCREMENT,
-  `NAME` varchar(100) NOT NULL,
+  `CUSTOMERID` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `FIRSTNAME` varchar(100) NOT NULL,
+  `LASTNAME` varchar(100) NOT NULL,
+  `USERNAME` varchar(20) NOT NULL,
+  `PASSWORD` varchar(20) NOT NULL,
   `AGE` int(10) unsigned NOT NULL,
-  PRIMARY KEY (`CUST_ID`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+  PRIMARY KEY (`CUSTOMERID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
 -- Dumping data for table `customer`
 --
-
 LOCK TABLES `customer` WRITE;
 /*!40000 ALTER TABLE `customer` DISABLE KEYS */;
 /*!40000 ALTER TABLE `customer` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `vehicle`
+--
+DROP TABLE IF EXISTS `vehicle`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `vehicle` (
+  `VEHICLEID` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `CUSTOMERID` int(10) unsigned NOT NULL,
+  `PLATENUMBER` varchar(100) NOT NULL,
+  PRIMARY KEY (`CUSTOMERID`,`PLATENUMBER`),
+  KEY `VEHICLEID` (`VEHICLEID`),
+  KEY `CUSTOMERID` (`CUSTOMERID`),
+  KEY `PLATENUMBER` (`PLATENUMBER`),
+  CONSTRAINT `vehicle_ibfk_1` FOREIGN KEY (`CUSTOMERID`) REFERENCES `customer` (`CUSTOMERID`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `vehicle`
+--
+LOCK TABLES `vehicle` WRITE;
+/*!40000 ALTER TABLE `vehicle` DISABLE KEYS */;
+/*!40000 ALTER TABLE `vehicle` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
@@ -49,4 +80,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2016-09-18 10:28:50
+-- Dump completed on 2016-09-26 12:30:31
